7516cc521196a4531f5b08e4a91e39ca438bd9c5
Wyatt Scott <122546418+WD-Scott@users.noreply.github.com>
2024-06-21 08:06:31 -0400

    Delete raven_counts.sh

--- raven_counts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Case-sensitive and case-insensitive  count of number of lines in The Raven containing 'raven'
-raven_lower=$(grep -c 'raven' books/poe_17192.txt)
-raven_title=$(grep -c 'Raven' books/poe_17192.txt)
-
-echo "Lines containing 'raven' (lowercase): $raven_lower"
-echo "Lines containing 'Raven' (title case): $raven_title"
